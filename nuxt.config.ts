086b47eea1c57d6f192abe322d91cebf9d4d925f
rhnas110 <raihanas110@gmail.com>
2024-10-23 23:59:00 +0700

    style: add rounded pagination button

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    devtools: { enabled: false },
-    modules: [
-        '@nuxtjs/tailwindcss',
-        "nuxt-lodash",
-        '@nuxt/image',
-    ],
+  ssr: false,
+  devtools: { enabled: false },
+
+  modules: [
+      '@nuxtjs/tailwindcss',
+      "nuxt-lodash",
+      '@nuxt/image',
+  ],
+
+  compatibilityDate: '2024-10-23',
 })
